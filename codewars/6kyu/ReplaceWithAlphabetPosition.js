bb9413742fb76fb83535c979c8d1ecd6dd9b9fb5
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-06-04 21:32:09 +0300

    Completed kata 'Replace With Alphabet Position'

---
+++ codewars/6kyu/ReplaceWithAlphabetPosition.js
@@ -0,0 +1,29 @@
+// Replace With Alphabet Position
+
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+a being 1, b being 2, etc.
+
+As an example:
+
+alphabet_position("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" as a string.
+
+*/
+
+function alphabetPosition(text) {
+  const lettersArr = text.toLowerCase().match(/[a-z]/g);
+  
+  if (lettersArr === null) {
+    return '';
+  }
+  
+  return lettersArr
+    .map(item => item.charCodeAt() - 'a'.charCodeAt() + 1)
+    .join(' ');
+}
